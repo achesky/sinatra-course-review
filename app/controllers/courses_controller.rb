06e9d336788eb2cb039bf04a56959969bf0d3375
Allison Chesky <afc340@nyu.edu>
2017-06-29 23:29:36 +0000

    added to courses controller

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -12,4 +12,21 @@ class CoursesController < ApplicationController
     erb :'courses/new'
   end
 
+  get "/courses/:id/edit" do
+    redirect_if_not_logged_in
+    @error_message = params[:error]
+    @course = Course.find(params[:id])
+    erb :'courses/edit'
+  end
+
+  post "/courses/:id" do
+    redirect_if_not_logged_in
+    @course = Course.find(params[:id])
+    unless Course.valid_params?(params)
+      redirect "/courses/#{@course.id}/edit?error=invalid course"
+    end
+    @course.update(params.select{|k|k=="professor" || k=="capacity"})
+    redirect "/courses/#{@course.id}"
+  end
+
 end
