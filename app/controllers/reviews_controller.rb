6ba99842d4c2a2fd6e95b1b95f4e8619031fb7ff
Allison Chesky <afc340@nyu.edu>
2017-06-29 23:45:10 +0000

    continued reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -11,4 +11,21 @@ class ReviewsController < ApplicationController
     erb :'reviews/new'
   end
 
+  get "/reviews/:id/edit" do
+    redirect_if_not_logged_in
+    @error_message = params[:error]
+    @review = Review.find(params[:id])
+    erb :'reviews/edit'
+  end
+
+  post "/reviews/:id" do
+    redirect_if_not_logged_in
+    @review = Review.find(params[:id])
+    unless Review.valid_params?(params)
+      redirect "/reviews/#{@review.id}/edit?error=invalid review"
+    end
+    @review.update(params.select{|k|k=="title" || k=="content" || k=="review_id"})
+    redirect "/reviews/#{@review.id}"
+  end
+
 end
